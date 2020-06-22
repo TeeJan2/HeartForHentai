6fa22514a64a8e71952dcf50635e408afe8c4a3c
TntTastisch <tnttastisch@gmail.com>
2020-06-22 23:08:22 +0200

    Optilizing

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-if(!isset($_SESSION['userid'])) {
-    die('Bitte zuerst <a href="login.php">einloggen</a>');
-}
-$userid = $_SESSION['userid'];
-
-?>
 <html lang="de" dir="ltr">
     <head>
         <meta charset="utf-8">
